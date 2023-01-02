a2c680deb07e84f0ff16b1b47ed5b7b5c94b8073
Thangam45 <120696116+Thangam45@users.noreply.github.com>
2023-01-02 10:13:14 +0530

    resume

---
+++ script7.js
@@ -0,0 +1,52 @@
+var head1=[ 
+    {
+        
+           careerobjective : "to be a part o fo company that indulges professional growth which provides challenging and rewaarding career whilw allowing me to utilize my knowledge and skills.",
+    },
+    {
+    summary :"i am good fun and friendly person, i am honest and punctual. i ahve good communication skill. i have creative mind and am always realy for new challenges. i can deal with difficulti situations. maybe a lot of people are more the good then me, but with perseverance and strong will can defeat all."
+    },
+    {
+        personalskills:"vreativity, interpersonal skills, critical thinking, problem solving, public speaking, customer service skills, teamwork skill, communication."
+    }
+    ]
+     var second =[
+    {
+        Qualification:'Bsc., Mathematics',
+        Schoolcollege:'AVC COLLEGE,AUTONOMOUS',
+        Board:'Bharathidasan university',
+        yearofpassing:'2021',
+        percentageCGPA:'85%'
+    },
+    {
+        Qualification:'HSC',
+        Schoolcollege:'MUNICIPAL HIGHER SECONDARY SCHOOL,MAYILADUTHURAI',
+        Board:'State board',
+        yearofpassing:'2019',
+        percentageCGPA:'74%'
+    },
+    {
+        Qualification:'SSLC',
+        Schoolcollege:'KSO HIGH SCHOOL,MANGANALLUR',
+        Board:'State board',
+        yearofpassing:'2016',
+        percentageCGPA:'83%'
+    }
+     ]
+    
+     var skills=
+        ["c","c++","java","HTML5", "CSS3", "JAVASCRIPT", "RDBMS,", "SQL", "ms office"]
+        var personalinfo={
+    Name:"E.Thangam",
+    Father:"T.Elangovan",
+    DOB:"27.07.2001",
+    Gender:"Male",
+    Materialstatus:"single",
+    Nationality:"indian",
+    Religion:"hindu",
+    Mobileno: 6383233292
+        }
+    console.log(head1)
+    console.table(second)
+    console.log(skills)
+    console.table(personalinfo)
